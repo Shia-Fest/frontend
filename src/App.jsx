10dea314f1b5b421bb8ae591d9011b5d0218247e
ajmalMundumuzhi <muhajmal73@gmail.com>
2025-09-12 11:32:51 +0530

    feat: initialize React + Vite app with tailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-gray-900 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Arts Fest - User Site
+      </h1>
+    </div>
+  )
+}
+
+export default App
