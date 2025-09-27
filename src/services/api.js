3e7d7415919d389717c0677202dcf9f27c37ab61
ajmalMundumuzhi <muhajmal73@gmail.com>
2025-09-27 21:19:52 +0530

    API_URL fixed

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://backend-jq71.onrender.com'
+const API_URL = 'https://backend-jq71.onrender.com/api'
 
 const api = axios.create({
     baseURL: API_URL,
